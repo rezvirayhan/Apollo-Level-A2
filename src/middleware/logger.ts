6e9f6f24ed062b115698aa326a1e78f8ced75d9e
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-06-01 16:33:18 +0600

    feat: logger text

---
+++ src/middleware/logger.ts
@@ -0,0 +1,10 @@
+import type { NextFunction, Request, Response } from "express";
+import fs from "fs";
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  const log = `\nMethod -> ${req.method} Time -> ${Date.now()}, URL -> ${req.url}\n`;
+  fs.appendFile("logger.txt", log, (err) => {});
+  next();
+};
+
+export default logger;
