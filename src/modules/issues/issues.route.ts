b94abf03f6ba7ffaaf9c6ed3ebfeee72320cfaeb
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-06-04 01:19:18 +0600

    feat: add issue route

---
+++ src/modules/issues/issues.route.ts
@@ -0,0 +1,35 @@
+import { Router } from "express";
+
+import { authMiddlewares } from "../../middleware/auth.middleware";
+import { roleMiddlewares } from "../../middleware/role.middleware";
+import { issueControllers } from "./issues.controller";
+
+const router = Router();
+
+router.get("/", issueControllers.getAll);
+router.get("/:id", issueControllers.getById);
+
+router.post(
+  "/",
+  authMiddlewares.auth,
+  roleMiddlewares.authorize("contributor", "maintainer"),
+  issueControllers.create,
+);
+
+router.patch(
+  "/:id",
+  authMiddlewares.auth,
+  roleMiddlewares.authorize("contributor", "maintainer"),
+  issueControllers.update,
+);
+
+router.delete(
+  "/:id",
+  authMiddlewares.auth,
+  roleMiddlewares.authorize("maintainer"),
+  issueControllers.remove,
+);
+
+export const issueRoutes = {
+  router,
+};
