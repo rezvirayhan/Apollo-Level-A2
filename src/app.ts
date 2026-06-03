2ea56f7a05a4516c75dd887cb2298cfbd9e4f6c5
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-06-04 00:47:24 +0600

    remove old auth

--- src/app.ts
+++ src/app.ts
@@ -5,28 +5,34 @@ import express, {
   type Request,
   type Response,
 } from "express";
+
 import { errorMiddlewares } from "./middleware/error.middleware";
 import logger from "./middleware/logger";
 import { notFoundMiddlewares } from "./middleware/notFound.middleware";
-import { authRoutes } from "./modules/auth/routes";
-import { issueRoutes } from "./modules/issues/routes";
+import router from "./routes";
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(
   cors({
     origin: "http://localhost:3000",
+    credentials: true,
   }),
 );
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ message: "Hello World!", author: "Next Level AI" });
-});
 
 app.use(logger);
-app.use("/api/auth", authRoutes.router);
-app.use("/api/issues", issueRoutes.router);
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "DevPulse API Running Successfully",
+  });
+});
+
+app.use("/api", router);
 
 app.use(notFoundMiddlewares.notFound);
 
