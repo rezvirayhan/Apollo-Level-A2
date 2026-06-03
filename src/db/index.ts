b26aee1711382d4ba7a46970b5a74632a25b9c6f
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-06-03 18:11:49 +0600

    feat: vercel added

--- src/db/index.ts
+++ src/db/index.ts
@@ -8,16 +8,16 @@ export const pool = new Pool({
 export const initDB = async () => {
   try {
     await pool.query(`
-     CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  password TEXT NOT NULL,
-  role VARCHAR(20) DEFAULT 'contributor',
-  created_at TIMESTAMP DEFAULT NOW(),
-  updated_at TIMESTAMP DEFAULT NOW()
-);
-    `);
+  CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role VARCHAR(20) DEFAULT 'contributor',
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+  );
+`);
 
     await pool.query(`
       id SERIAL PRIMARY KEY,
