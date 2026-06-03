82993840c7e36116f3df5d61cef19b714126261f
Rezvi Rayhan <107808969+rezvirayhan@users.noreply.github.com>
2026-06-04 01:19:03 +0600

    feat: add issue interface

---
+++ src/modules/issues/issues.interface.ts
@@ -0,0 +1,26 @@
+export type IssueType = "bug" | "feature_request";
+export type IssueStatus = "open" | "in_progress" | "resolved";
+
+export interface IIssue {
+  id: number;
+  title: string;
+  description: string;
+  type: IssueType;
+  status: IssueStatus;
+  reporter_id: number;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface ICreateIssue {
+  title: string;
+  description: string;
+  type: IssueType;
+}
+
+export interface IUpdateIssue {
+  title?: string;
+  description?: string;
+  type?: IssueType;
+  status?: IssueStatus;
+}
